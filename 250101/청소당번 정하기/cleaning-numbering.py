485787a6608d3acafe6cbd6c6569c1053e24fc89
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-01-01 13:34:38 +0900

    Title: 청소당번 정하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250101/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+classroom = 0
+corridor = 0
+bathroom = 0
+
+for i in range(1, n):
+    if i%12==0:
+        bathroom+=1
+    elif i%3==0:
+        corridor+=1
+    elif i%2==0:
+        classroom+=1
+
+print(classroom, corridor, bathroom)
