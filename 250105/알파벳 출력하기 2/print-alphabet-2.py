b34a8e262bb2cc968497be74987c0e91f80710e7
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-01-05 21:26:14 +0900

    Title: 알파벳 출력하기 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250105/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+cnt = 'A'
+	
+# 알파벳을 삼각형 모양으로 출력합니다.
+for i in range(n):
+    for j in range(n-i):
+        print(" ", end=" ")
+	for _ in range(i + 1):
+		print(cnt, end=" ")
+		cnt = chr(ord(cnt) + 1)
+		if ord(cnt) > ord('Z'):
+			cnt = 'A'
+	print()
