156017844a6eb0b7aa359031b0c83911de2829ae
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-01-08 23:17:34 +0900

    Title: 특정 숫자 도달하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250108/특정 숫자 도달하기/reaching-specific-number.py
+++ 250108/특정 숫자 도달하기/reaching-specific-number.py
@@ -6,13 +6,13 @@ n = input().split()
 
 for i in range(len(n)):
     if int(n[i]) >=250:
-        print(num_sum, num_sum/cnt)
+        print(num_sum, f"{num_sum/cnt:.1f}")
         break
     arr.append(n[i])
     num_sum+=int(n[i])
     cnt+=1
     if cnt == 10:
-        print(num_sum, num_sum/cnt)
+        print(num_sum, f"{num_sum/cnt:.1f}")
     
     
         
