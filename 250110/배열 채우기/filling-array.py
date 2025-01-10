2cb8899cd1ac9b84229e15cb0a2209f5b39efb8a
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-01-10 20:58:20 +0900

    Title: 배열 채우기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250110/배열 채우기/filling-array.py
+++ 250110/배열 채우기/filling-array.py
@@ -1,9 +1,12 @@
 arr = input().split()
 cnt=0
+arr1 = []
 for i in range(len(arr)):
     if arr[i] == "0":
-        arr.pop()
-        print(*arr[::-1])
+        print(*arr1[::-1])
+        break
+    else:
+        arr1.append(arr[i])
     cnt+=1
     if cnt == len(arr):
-        print(*arr[::-1])
+        print(*arr1[::-1])
