7b25e210c55ff11ca596dcb2454566f0a833e24f
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-01-15 22:27:39 +0900

    Title: 연속부분수열일까, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250115/연속부분수열일까/contiguous-array-or-not.py
+++ 250115/연속부분수열일까/contiguous-array-or-not.py
@@ -4,7 +4,7 @@ n1arr = input().split()
 n2arr = input().split()
 cnt = 0
 
-for i in range(n1-n2):
+for i in range(n1-n2+1):
     if n1arr[i] == n2arr[0]:
         cnt+=1
         for j in range(1, n2):
