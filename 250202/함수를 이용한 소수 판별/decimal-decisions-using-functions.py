a1ed758d933e01320e0490ba8bf4ec92ab81e799
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-02 11:23:37 +0900

    Title: 함수를 이용한 소수 판별, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250202/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+# Write your code here!
+def is_sosu(n):
+    for i in range(2, n):
+        if n%i==0:
+            return False
+    return True
+
+
+def ans(a,b):
+    result = 0
+    for i in range(a, b+1):
+        if is_sosu(i):
+            result+=i
+    print(result)
+
+ans(a,b)
