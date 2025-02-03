808b22f9cd263b4b6217e492f8498c8d81c9def1
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-03 17:40:32 +0900

    Title: 두 정수에 대한 연산값, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250203/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 250203/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -3,7 +3,9 @@ a, b = map(int, input().split())
 # Write your code here!
 def ans(a,b):
     if a>b: # b가 항상 크다고 가정
-        a,b = b,a
+        a+=25
+        b*=2
+        return a,b
     a*=2
     b+=25
     return a, b
