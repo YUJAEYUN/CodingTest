b0d9c96070c2e2886fb447ed3e33702ceac452dd
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-06 15:32:17 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250206/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# Write your code here!
+def recursive(n):
+
+    if n == 1:
+        return 0
+    if n%2==0:
+        return recursive(n//2) + 1
+    else:
+        return recursive(n*3+1) + 1
+
+print(recursive(n))
