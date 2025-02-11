e3d76ee93210630e4c6c62189412be994b5e732e
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-11 16:58:11 +0900

    Title: Time to Time, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250211/Time to Time/time-to-time.py
@@ -0,0 +1,18 @@
+a, b, c, d = map(int, input().split())
+
+# Write your code here!
+elapsed_time = 0
+hour, mins = a, b
+while True:
+
+    if hour == c and mins == d:
+        break
+
+    elapsed_time+=1
+    mins+=1
+
+    if mins == 60:
+        hour+=1
+        mins=0
+
+print(elapsed_time)
