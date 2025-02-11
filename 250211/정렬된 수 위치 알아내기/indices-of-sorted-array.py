8b75705f2015b88f621c805af9faec78aa8c8cbb
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-11 16:40:59 +0900

    Title: 정렬된 수 위치 알아내기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250211/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,19 @@
+
+n = int(input())
+sequence = list(map(int, input().split()))
+li = [0 for _ in range(n)]
+# Write your code here!
+idx = 0
+cnt=1
+for j in range(n):
+    minimum = 1001
+    for i in range(n):
+        if sequence[i]<minimum:
+            idx = i
+            minimum = sequence[i]
+    sequence[idx] = 1001
+    li[idx] = cnt
+    cnt+=1
+        
+print(*li)
+
