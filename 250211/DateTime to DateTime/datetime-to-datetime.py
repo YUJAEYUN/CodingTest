d7edbc40a44ab8a2abc67e87f0894afb13f51dbb
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-11 17:13:33 +0900

    Title: DateTime to DateTime, Time: 5102ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 250211/DateTime to DateTime/datetime-to-datetime.py
+++ 250211/DateTime to DateTime/datetime-to-datetime.py
@@ -5,9 +5,11 @@ elapsed_time = 0
 day, hour, mins = 11,11,11
 
 while True:
-    if a <= 11 and b <= 11 and c < 11:
-        print("-1")
-        break
+    if a <= 11:
+        if b<=11:
+            if c<11:
+                print("-1")
+                break
     if hour==b and mins == c and day==a:
         print(elapsed_time)
         break
