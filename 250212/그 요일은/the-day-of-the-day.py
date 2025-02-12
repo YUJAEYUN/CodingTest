690088259e5e22004b0832e931b2237b46e11c42
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-12 19:23:26 +0900

    Title: 그 요일은, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250212/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,32 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Write your code here!
+month, day = m1, d1
+elapsed_days = 0
+
+while True:
+    if month == m2 and day == d2:
+        break
+    elapsed_days+=1
+    day+=1
+    if month == 2:
+        if day == 30:
+            month+=1
+            day= 1
+
+    elif month ==1 or month==3 or month==5 or month==7or month==8 or month==10 or month==12:
+        if day==32:
+            month+=1
+            day=1
+    else:
+        if day==31:
+            month+=1
+            day=1
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+cnt = elapsed_days//7
+if A == days[elapsed_days%7]:
+    cnt+=1
+
+print(cnt)
