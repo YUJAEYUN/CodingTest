8f8813a72925932203ea782fb55cd66074cc8b25
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-13 17:38:23 +0900

    Title: 왔다 갔던 구역 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250213/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250213/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,17 +7,17 @@ for _ in range(n):
     xi, di = input().split()
     xi = int(xi)
     if di=="L":
-        for i in range(move, move-xi, -1):
+        for i in range(move-1, move-xi-1, -1):
             moves[i]+=1
         move-=xi
     else:
-        for i in range(move, move+xi):
+        for i in range(move+1, move+xi+1):
             moves[i]+=1
         move+=xi
 
 cnt=0
 for _ in range(len(moves)):
-    if moves[_] > 1:
+    if moves[_]>1:
         cnt+=1
-
 print(cnt)
+
