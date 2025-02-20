b279381307c17de161ac56c84ad5c79cd4aa2ba7
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-20 14:55:59 +0900

    Title: 선두를 지켜라, Time: 91ms, Memory: 33MB, Status: Wrong Answer - CodeTree

---
+++ 250220/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,23 @@
+N, M = map(int,input().split())
+MAX_T = 1000000
+arr_a = [0] * (MAX_T+1)
+arr_b = [0] * (MAX_T+1)
+cnt = 0
+time_a = 0
+time_b = 0
+for i in range(N):
+    v,t = input().split()
+    time_a += int(t)
+    for i in range(time_a-int(t)+1, time_a+1):
+        arr_a[i] = int(v) * (i)
+
+for j in range(M):
+    v,t = input().split()
+    time_b += int(t)
+    for i in range(time_b-int(t)+1, time_b+1):
+        arr_b[i] = int(v) * (i)
+
+for i in range(1, len(arr_a)-1):
+    if (arr_a[i] - arr_b[i]) * (arr_a[i+1] - arr_b[i+1]) < 0:
+        cnt+=1
+print(cnt)
