c67c61412c60283e010d46640c226e9ba4b222c7
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-20 15:01:17 +0900

    Title: 선두를 지켜라, Time: 99ms, Memory: 34MB, Status: Wrong Answer - CodeTree

--- 250220/선두를 지켜라/keep-the-lead.py
+++ 250220/선두를 지켜라/keep-the-lead.py
@@ -9,13 +9,13 @@ for i in range(N):
     v,t = input().split()
     time_a += int(t)
     for i in range(time_a-int(t)+1, time_a+1):
-        arr_a[i] = int(v) * (i)
+        arr_a[i] = arr_a[i-1] + int(v) * (i)
 
 for j in range(M):
     v,t = input().split()
     time_b += int(t)
     for i in range(time_b-int(t)+1, time_b+1):
-        arr_b[i] = int(v) * (i)
+        arr_b[i] = arr_b[i-1] + int(v) * (i)
 
 for i in range(1, len(arr_a)-1):
     if (arr_a[i] - arr_b[i]) * (arr_a[i+1] - arr_b[i+1]) < 0:
