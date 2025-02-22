fd2440ac4c3c04175c878ac188a0916a8f94a1c6
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-22 13:22:27 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250222/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,36 @@
+class Shake:
+    def __init__(self, time, person1, person2):
+        self.time, self.person1, self.person2 = time, person1, person2
+
+n,k,p,t = tuple(map(int,input().split()))
+shakes = []
+for _ in range(t):
+    time, person1, person2 = tuple(map(int,input().split()))
+    shakes.append(Shake(time,person1,person2))
+
+shake_num = [0] * (n+1)
+infected = [False] * (n+1)
+
+infected[p] = True
+
+shakes.sort(key=lambda x:x.time)
+
+for shake in shakes:
+    target1 = shake.person1
+    target2 = shake.person2
+
+    if infected[target1]:
+        shake_num[target1] +=1
+    if infected[target2]:
+        shake_num[target2] +=1
+
+    if shake_num[target1] <= k and infected[target1]:
+        infected[target2] = True
+    if shake_num[target2] <= k and infected[target2]:
+        infected[target1] = True
+
+for i in range(1, n+1):
+    if infected[i]:
+        print(1,end="")
+    else:
+        print(0, end="")
