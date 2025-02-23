925029f2e2bf5478f9032adfb9a5f248b072b015
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-23 21:48:50 +0900

    Title: 작은 구슬의 이동, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250223/작은 구슬의 이동/small-marble-movement.py
+++ 250223/작은 구슬의 이동/small-marble-movement.py
@@ -15,13 +15,12 @@ else:
 dxs,dys = [0,1,-1,0], [1,0,0,-1] #북동서남 
 
 def in_range(c,r):
-    return 0<=r and r<n and 0<=c and c<n
+    return 1<=r and r<n+1 and 1<=c and c<n+1
 
-for i in range(t+1):
+for i in range(t):
     nc,nr = c+dxs[dir_num], r+dys[dir_num]
     if not in_range(nc,nr):
         dir_num=3-dir_num
-        pass
+        continue
     c,r = c+dxs[dir_num], r+dys[dir_num]
-
-print(r, c)
+print(r,c)
