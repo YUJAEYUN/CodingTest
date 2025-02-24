bf96fdfa4ad37cbc264cc902a8d3376e25220e4f
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-02-24 18:38:34 +0900

    Title: 격자 위의 편안한 상태, Time: 117ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 250224/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 250224/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,24 +1,21 @@
 N, M = map(int,input().split())
 
-floor = [[0] * (N) for _ in range(N)]
-dx, dy = [-1, 0, 1, 0], [0, -1, 0, 1]
-
-def is_range(x, y):
-    return 0 <= x < N and 0 <= y < N
-
-def is_check(a, b):
+floor = [[0] * (N+2) for _ in range(N+2)]
+    
+def is_check(r,c):
+    dx,dy = [-1,0,1,0], [0,-1,0,1]
     cnt = 0
-    for i in range(4):  # i 사용
-        nx, ny = a + dx[i], b + dy[i]
-        if is_range(nx, ny) and floor[nx][ny] == 1:
-            cnt += 1
-    return cnt == 3
-
+    for i in range(4):
+        if floor[r+dx[i]][c+dy[i]] == 1:
+            cnt+=1
+        if cnt==3:
+            return True
+    return False
 
 for i in range(M):
     r,c = map(int,input().split())
-    floor[r-1][c-1] = 1
-    if is_check(r-1,c-1):
+    floor[r][c] = 1
+    if is_check(r,c):
         print('1')
     else:
         print('0')
