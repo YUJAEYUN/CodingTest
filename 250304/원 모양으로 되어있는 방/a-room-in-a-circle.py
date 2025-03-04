620bc4ed2b25cf55b5deab97632c9d973ca86cc4
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-04 10:49:29 +0900

    Title: 원 모양으로 되어있는 방, Time: 71ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250304/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = [int(input()) for _ in range(n)]
+min_ans = 9999999
+# Please write your code here.
+for i in range(n):
+    ans = 0
+    for j in range(n):
+        if i <= j:
+            ans += a[j] * (j-i)
+        else:
+            ans += a[j]*((j-i)+len(a))
+    min_ans = min(ans, min_ans)
+print(min_ans)
+    
+
