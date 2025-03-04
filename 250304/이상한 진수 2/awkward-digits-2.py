da4822505e3d561ed31ad139499bf04213d97bb0
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-04 10:00:30 +0900

    Title: 이상한 진수 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250304/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,18 @@
+a = list(input())
+
+for i in range(len(a)):
+    if a[i] == '0':
+        a[i] = '1'
+        break
+# Please write your code here.
+def binary_to_decimal(arr):
+    cnt = 0
+    sum = 0
+    for i in arr[::-1]:
+        sum += int(i)*(2**cnt)
+        cnt+=1
+    return sum
+
+ans = binary_to_decimal(a)
+print(ans)
+    
