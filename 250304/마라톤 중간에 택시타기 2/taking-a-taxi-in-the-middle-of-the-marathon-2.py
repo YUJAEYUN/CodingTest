95000fe67cceb77cfc68d3c0abe513dec2e72959
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-04 10:26:08 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250304/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+# Please write your code here.
+min_distance = 9999999
+
+for i in range(1,n-1):
+    point_x, point_y = 0,0
+    distance = x[point_x] + y[point_y]
+
+    for j in range(1,n):
+        if i == j:
+            pass
+        else:
+            distance += abs(x[j]-x[point_x]) + abs(y[j] - y[point_y])
+            point_x = j
+            point_y = j
+    min_distance = min(min_distance, distance)
+print(min_distance)
