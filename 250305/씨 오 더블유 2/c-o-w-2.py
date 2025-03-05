98604dd185ae64e616d0d3b69f6fbb38a1288652
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-05 16:02:11 +0900

    Title: 씨 오 더블유 2, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 250305/씨 오 더블유 2/c-o-w-2.py
+++ 250305/씨 오 더블유 2/c-o-w-2.py
@@ -3,8 +3,8 @@ str = input()
 cnt=0
 # Please write your code here.
 for i in range(n-2):
-    for j in range(n-1):
-        for k in range(n):
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
             if str[i]=='C' and str[j]=='O' and str[k]=='W':
                 cnt+=1
 
