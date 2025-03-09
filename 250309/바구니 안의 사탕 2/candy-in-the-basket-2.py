56633f6767e5b29f3c8d5b13f6d547e72c461af8
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-09 11:23:10 +0900

    Title: 바구니 안의 사탕 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250309/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,16 +1,23 @@
+import sys
+
 N, K = map(int, input().split())
 MAX_LEN = 100
+max_candy = 0
+total_candy = 0
 
 arr = [0] * (MAX_LEN+1)
 
 for _ in range(N):
     candy, pos = map(int,input().split())
     arr[pos] += candy # 제한조건 같은 위치에 여러 바구니가 놓여 있는 것이 가능합니다...를 못봄
-
+    total_candy += candy
 # Please write your code here.
-max_candy = 0
 
-for i in range(K, MAX_LEN-K): # 중앙값의 인덱스는 i
+if K >= 50:
+    print(total_candy)
+    sys.exit()
+
+for i in range(K, MAX_LEN-K+1): # 중앙값의 인덱스는 i
     candy_count = 0
     tmp_arr = arr[i-K:i+K+1] # 이거 자꾸 빼먹네 슬라이싱 끝값은 포함 안되서 +1 해야함.......
 
