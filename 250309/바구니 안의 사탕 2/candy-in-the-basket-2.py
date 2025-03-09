e2bbe9a515e179ec6187c323608856ed97d56c42
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-03-09 11:07:31 +0900

    Title: 바구니 안의 사탕 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250309/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,24 @@
+N, K = map(int, input().split())
+MAX_LEN = 100
+
+arr = [0] * (MAX_LEN+1)
+
+for _ in range(N):
+    candy, pos = map(int,input().split())
+    arr[pos] = candy
+
+# Please write your code here.
+max_candy = 0
+
+for i in range(K, MAX_LEN-K+1): # 중앙값의 인덱스는 i
+    candy_count = 0
+    tmp_arr = arr[i-K:i+K+1] # 이거 자꾸 빼먹네 슬라이싱 끝값은 포함 안되서 +1 해야함.......
+
+    for candys in tmp_arr:
+        candy_count += candys
+    
+    max_candy = max(max_candy, candy_count)
+
+print(max_candy)
+
+
