7fa67c026fcbcae5853e07f121894dc999e16351
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-06-21 00:40:47 +0900

    Title: 입력받은 값 교체하기, Time: 195ms, Memory: 15MB, Status: Passed - Codetree

--- 250621/입력받은 값 교체하기/changing-inputs.js
+++ 250621/입력받은 값 교체하기/changing-inputs.js
@@ -4,4 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-console.log(b, a)
+//값 교체 파이썬이랑 비슷
+[a,b] = [b,a]
+
+console.log(a, b)
