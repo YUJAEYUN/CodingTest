e3e78ec6e0430778d2ed0bd2d9aeac0dfb38bb2a
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-06-23 18:57:51 +0900

    Title: 점수 비교, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250623/점수 비교/score-comparison.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let A = input[0].split(" ");
+let B = input[1].split(" ");
+
+let mathA = Number(A[0]), engA = Number(A[1]);
+let mathB = Number(B[0]), engB = Number(B[1]);
+
+if (mathA > mathB && engA > engB){
+    console.log(1);
+}
+else{
+    console.log(0);
+}
