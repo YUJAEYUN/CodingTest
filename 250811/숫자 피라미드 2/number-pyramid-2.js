ef080050bb791bafc887f6f1ad187f5a8d70a49d
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2025-08-11 23:15:04 +0900

    Title: 숫자 피라미드 2, Time: 196ms, Memory: 15MB, Status: Passed - Codetree

--- 250811/숫자 피라미드 2/number-pyramid-2.js
+++ 250811/숫자 피라미드 2/number-pyramid-2.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 let input = fs.readFileSync(0).toString().trim();
 
-let N = Number(input[0]);
+let N = Number(input); // input은 문자열이므로 Number()를 사용하여 숫자로 변환합니다.
 let num = 1;
 
-for (let i=1;i<=N;i++){
-    let result="";
-    for (let j=0; j<i; j++){
-        result += `${num} `
-        num+=1;
+for (let i = 1; i <= N; i++) {
+    let result = ""; // result 변수를 각 반복마다 초기화해줍니다.
+    for (let j = 0; j < i; j++) {
+        result += `${num} `;
+        num++;
     }
     console.log(result);
 }
