2acdc32287d6ee1d3676898b7bbaa3ab461c3786
YUJAEYUN <86181002+YUJAEYUN@users.noreply.github.com>
2026-04-02 16:36:30 +0900

    Title: 스승의 은혜 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 260402/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -1,20 +1,23 @@
 N, B = map(int, input().split())
 P = [int(input()) for _ in range(N)]
 P.sort()
-# Please write your code here.
 ans = 0
 
 for i in range(N):
     tmp = 0
     cnt = 0
+
+    price = P[:]
+    price[i] //= 2
+    price.sort()
+
     for j in range(N):
-        if tmp < B:
-            cnt +=1
-            if i == j:
-                tmp += P[j]//2
-            else:
-                tmp += P[j]
-        if tmp > B:
-            cnt-=1
-    ans = max(ans,cnt)
+        if tmp + price[j] <= B:
+            tmp += price[j]
+            cnt += 1
+        else:
+            break
+
+    ans = max(ans, cnt)
+
 print(ans)
